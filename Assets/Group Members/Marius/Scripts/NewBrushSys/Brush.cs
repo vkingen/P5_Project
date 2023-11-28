641c5c02faea6b0a70204bcb3c3855c62b1cd85a
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2023-11-28 13:22:35 +0100

    Eraser V.1.0.0.1

--- Assets/Group Members/Marius/Scripts/NewBrushSys/Brush.cs
+++ Assets/Group Members/Marius/Scripts/NewBrushSys/Brush.cs
@@ -13,6 +13,9 @@ public class Brush : MonoBehaviour
 
     private BrushStroke _activeBrushStroke;
 
+    //A list to hold the gameobjects instantiated, in order to be able to delete them again
+    public List<GameObject> sceneObjects = new List<GameObject>(); 
+
     private void Update() 
     {
         // Get the position & rotation of the point from which we draw
@@ -26,6 +29,7 @@ public class Brush : MonoBehaviour
         if (drawTriggerPressed && _activeBrushStroke == null) {
             // Instantiate a copy of the Brush Stroke prefab.
             GameObject brushStrokeGameObject = Instantiate(_brushStrokePrefab);
+            sceneObjects.Add(brushStrokeGameObject);
 
             // Grab the BrushStroke component from it
             _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke>();
@@ -42,6 +46,73 @@ public class Brush : MonoBehaviour
         if (!drawTriggerPressed && _activeBrushStroke != null) {
             _activeBrushStroke.EndBrushStrokeWithBrushTipPoint(originPos, originRot);
             _activeBrushStroke = null;
+
+            //Tilføjet af Marius
+            /*MeshCollider collider = _brushStrokePrefab.GetComponent<MeshCollider>();
+            Mesh meshComponent = _brushStrokePrefab.transform.GetComponentInChildren<Mesh>();
+            collider.sharedMesh = meshComponent;
+            collider.convex = true;
+            collider.isTrigger = true;*/
+
+            string gameObjectName = "BrushStroke(Clone)";
+
+            if (sceneObjects != null)
+            {
+                foreach (GameObject sceneObject in sceneObjects)
+                {
+                    MeshCollider collider = sceneObject.GetComponent<MeshCollider>();
+
+                    // Check if the MeshCollider is not already present
+                    if (collider == null)
+                    {
+                        // If not, add a new MeshCollider
+                        collider = sceneObject.AddComponent<MeshCollider>();
+                    }
+
+                    Transform child = sceneObject.transform.GetChild(0);
+
+                    //MeshFilter meshFilter = sceneObject.GetComponentInChildren<MeshFilter>();
+
+                    //Transform meshFilter2 = meshFilter.transform.GetChild(0);
+                    MeshFilter meshFilter = child.GetComponent<MeshFilter>();
+
+                    print(meshFilter.gameObject.name);
+
+                    // Check if a MeshFilter is found
+                    if (meshFilter != null)
+                    {
+                        Mesh meshComponent = meshFilter.sharedMesh;
+
+                        // Check if a mesh is found in the MeshFilter
+                        if (meshComponent != null)
+                        {
+                            // Set the sharedMesh of the MeshCollider
+                            collider.sharedMesh = meshComponent;
+                            collider.convex = true;
+                            collider.isTrigger = true;
+                        }
+                        else
+                        {
+                            Debug.LogError("Mesh not found in MeshFilter.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("MeshFilter not found.");
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("GameObject with name '" + gameObjectName + "' not found in the scene.");
+            }
+
+            /*MeshCollider collider = _brushStrokePrefab.AddComponent<MeshCollider>();
+            Mesh meshComponent = _brushStrokePrefab.GetComponentInChildren<MeshFilter>().sharedMesh;
+            collider.sharedMesh = meshComponent;
+            collider.convex = true;
+            collider.isTrigger = true;*/
+
         }
     }
 }
