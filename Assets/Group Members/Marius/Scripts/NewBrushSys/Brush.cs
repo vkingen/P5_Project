f2b0280b5085d791303505a89767ad1eddd417ce
Marius-Arnbjerg <marius.arnbjerg@gmail.com>
2023-12-01 15:34:08 +0100

    Update Brush.cs

--- Assets/Group Members/Marius/Scripts/NewBrushSys/Brush.cs
+++ Assets/Group Members/Marius/Scripts/NewBrushSys/Brush.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,17 +15,24 @@ public class Brush : NetworkBehaviour
 
     private BrushStroke1 _activeBrushStroke;
 
+    private ulong brushStrokeInstanceId = 0;
+
+
+
     //A list to hold the gameobjects instantiated, in order to be able to delete them again
     //public List<GameObject> sceneObjects = new List<GameObject>(); 
 
     [ServerRpc (RequireOwnership = false)]
-    private void SpawnMesh_ServerRpc(Vector3 pos, Quaternion rot)
+    private void SpawnMesh_ServerRpc(Vector3 pos, Quaternion rot, ServerRpcParams serverRpcParams = default)
     {
-
+        
         GameObject brushStrokeGameObject = Instantiate(_brushStrokePrefab);
 
         NetworkObject networkObject = brushStrokeGameObject.GetComponent<NetworkObject>();
         networkObject.Spawn();
+        
+        
+        
 
         // Grab the BrushStroke component from it
         _activeBrushStroke = brushStrokeGameObject.GetComponent<BrushStroke1>();
@@ -33,15 +41,41 @@ public class Brush : NetworkBehaviour
         _activeBrushStroke.Initialize(pos, rot);
         // _activeBrushStroke.BeginBrushStrokeWithBrushTipPoint(pos, rot);
 
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        
+
+        // NOTE! In case you know a list of ClientId's ahead of time, that does not need change,
+        // Then please consider caching this (as a member variable), to avoid Allocating Memory every time you run this function
+        ClientRpcParams clientRpcParams = new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+                TargetClientIds = new ulong[] { clientId }
+            }
+        };
+
+        var id = networkObject.NetworkObjectId;
+        Debug.Log("OUR KEY: " + id);
+        SetLocalBrushStrokeInstanceId_ClientRpc((ulong) id, clientRpcParams);
 
         // InitializeBrushStroke(brushStrokeGameObject, pos, rot);
     }
 
-    
-    private void InitializeBrushStroke(GameObject bs, Vector3 pos, Quaternion rot)
+    [ClientRpc]
+    public void SetLocalBrushStrokeInstanceId_ClientRpc(ulong id, ClientRpcParams clientRpcParams)
     {
+        brushStrokeInstanceId = id;
+        if(!NetworkManager.SpawnManager.SpawnedObjects.ContainsKey(id))
+        {
+            Debug.LogError("DIDN?T HAVE KEY " + id);
+            return;
+        }
+        else
+        {
+            Debug.Log("WE CONTAIN KEY " + id);
+            _activeBrushStroke = NetworkManager.SpawnManager.SpawnedObjects[id].GetComponent<BrushStroke1>();
 
-        
+        }
     }
 
     private void Update() 
