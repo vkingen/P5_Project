f80f94acfb49dd853f575e6c8ae6b513d85fc072
Cath13 <criisj21@student.aau.dk>
2023-12-02 21:50:10 +0100

    drum

---
+++ Assets/Group Members/Cath/MusicalInstrument/Scripts/DrumStickInteractor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumStickInteractor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    OnCollissionEnter(Collider collider)
+    {
+        if (collider.tag == "Drum")
+        {
+        audio = GetComponent<AudioSource>;
+        audio.Play;
+        }
+        return;
+    }
+}
