7f2d5e928502ecb3bf78411a4372b049ec5ecfd7
Cath13 <criisj21@student.aau.dk>
2023-12-03 01:34:22 +0100

    drum set WORKS!!

--- Assets/Group Members/Cath/MusicalInstrument/Scripts/DrumStickInteractor.cs
+++ Assets/Group Members/Cath/MusicalInstrument/Scripts/DrumStickInteractor.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class DrumStickInteractor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
-    OnCollissionEnter(Collider collider)
-    {
-        if (collider.tag == "Drum")
-        {
-        audio = GetComponent<AudioSource>;
-        audio.Play;
-        }
-        return;
-    }
+    //}
+    //OnCollissionEnter(Collider collider)
+    //{
+    //    if (collider.tag == "Drum")
+    //    {
+    //    audio = GetComponent<AudioSource>;
+    //    audio.Play;
+    //    }
+    //    return;
+    //}
 }
