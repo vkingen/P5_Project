e243fc5dff6fe1a865c3c745eab70cb97ef8b266
norhady1 <norhady1@gmail.com>
2023-12-14 13:33:47 +0100

    radioplayer

--- Assets/Group Members/Nor/Script/VideoPlay.cs
+++ Assets/Group Members/Nor/Script/VideoPlay.cs
@@ -10,6 +10,15 @@ public class VideoPlay : MonoBehaviour
     [SerializeField] Slider volumeSlider;
 
     public VideoClip[] videoClips;
+<<<<<<< HEAD
+    public TMP_Text currentMinutes;
+    public TMP_Text currentSeconds;
+    public TMP_Text totalMinutes;
+    public TMP_Text totalSeconds;
+
+    public PlayHeadMover playHeadmover;
+=======
+>>>>>>> parent of 934ecd0 (lyd)
 
 
     private VideoPlayer videoPlayer;
@@ -21,6 +30,22 @@ public class VideoPlay : MonoBehaviour
         videoPlayer = GetComponent<VideoPlayer>();
     }
 
+<<<<<<< HEAD
+    public void Start()
+    {
+        videoPlayer.targetTexture.Release();
+    }
+    public void Update()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            SetCurrentTimeUI();
+            playHeadmover.MovePlayHead(CalculatePlayedFraction());
+        }
+    }
+
+=======
+>>>>>>> parent of 934ecd0 (lyd)
     public void SetNextClip()
     {
         videoClipIndex++;
@@ -33,7 +58,9 @@ public class VideoPlay : MonoBehaviour
 
         videoPlayer.clip = videoClips[videoClipIndex];
         videoPlayer.Play();
+        TotalTimeUI();
     }
+
 
     public void SetPreviousClip()
     {
@@ -65,5 +92,33 @@ public class VideoPlay : MonoBehaviour
 
     }
 
+<<<<<<< HEAD
+    void SetCurrentTimeUI()
+    {
+        string minutes = Mathf.Floor((int)videoPlayer.time / 60).ToString("00");
+        string seconds = ((int)videoPlayer.time % 60).ToString("00");
+
+        currentMinutes.text = minutes;
+        currentSeconds.text = seconds;
+    }
+
+
+    void TotalTimeUI()
+    {
+        string minutes = Mathf.Floor((int)videoPlayer.clip.length / 60).ToString("00");
+        string seconds = ((int)videoPlayer.clip.length % 60).ToString("00");
+
+        totalMinutes.text = minutes;
+        totalSeconds.text = seconds;
+    }
+
+    double CalculatePlayedFraction()
+    {
+        double fraction = (double)videoPlayer.frame / (double)videoPlayer.clip.frameCount;
+        return fraction;
+    }
+
+=======
+>>>>>>> parent of 934ecd0 (lyd)
 
 }
